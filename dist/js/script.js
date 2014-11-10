5e876aaf0c767fc24c3343f6405be2109933ad44
kaizer1v <kaizer1v@gmail.com>
2014-11-10 18:05:33 +0530

    added less classes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,1 @@
 // your script here.
-
-var globalVar = "I am Global";
-
-function one(param1) {
-  var inOne = "I am local";
-  console.log(inOne);
-
-  two(inOne);    
-}
-
-function two(param2) {
-    console.log(param2);
-}
